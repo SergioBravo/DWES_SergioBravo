e048d38afcac1f5f103607a6c0444d6977fec425
SergioBravo <90683590+SergioBravo@users.noreply.github.com>
2021-10-01 13:37:53 +0200

    Add files via upload

--- DWES-PHP/bolsa.php
+++ DWES-PHP/bolsa.php
@@ -4,9 +4,11 @@
     echo "<body>";
       #PRINCIPIO CODIGO
       for ($i=0; $i < 50; $i++) {//Creamos el array de la bolsa de forma dinamica
-        $bolsa = array("Empresa$i" ==> array());
+        $nombre = "empresa".($i+1);
+        $bolsa[$nombre] = array();
         for ($x=0; $x < 9 ; $x++) {
-          $bolsa[$i][$x] = array("valor$x" ==> rand(0, 15000));
+          $atributo = "valor".$x;
+          $bolsa[$nombre][$atributo] = rand(0, 15000);
         }
       }
 
@@ -14,16 +16,17 @@
       echo "<table border=1>";
         echo "<legend><h2>Tabla de atributos de cada empresa en la bolsa</h2></legend>";
         echo "<tr>";
-          echo "<th>Empresa</th>";echo "<th>Precio</th>";echo "<th>Valor</th>";echo "<th>Variación</th>";echo "<th>Npi</th>";
+          echo "<th>Empresa</th>";
+        for ($i=0; $i < count($bolsa["empresa".($i+1)]); $i++) {
+          echo "<th>Valor".($i+1)."</th>";
+        }
         echo "</tr>";
         foreach ($bolsa as $empresa => $atributos) {
           echo "<tr>";
             echo "<td>$empresa</td>";
-          foreach ($atributos as $atributo => $valor) {
-            if ($atributo == "precio") echo "<td width=50>-$valor %</td>";
-            elseif ($atributo == "valor") echo "<td width=50>$valor €</td>";
-            else echo "<td width=50>$valor</td>";
-          }
+            foreach ($atributos as $atributo => $valor) {
+              echo "<td width=50>$valor</td>";
+            }
           echo "</tr>";
         }
       echo "</table>";
@@ -31,6 +34,5 @@
       #FINAL CODIGO
     echo "</body>";
   echo "</html>";
+
  ?>
- $bolsa = array ("inditex" => array("valor1" => 0.07,"valor2" => 0.10,"valor3" => 100000,"valor4" => 15000000000,"valor5" => 15000000000,"valor6" => 15000000000,"valor7" => 15000000000,"valor8" => 15000000000,"valor9" => 15000000000),
-                 "Bbva" => array("precio" => 9,"valor" => 10,"variacion" => 8,"npi" => 9));
