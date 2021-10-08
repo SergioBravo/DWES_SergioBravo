96e6676861b8b21c350d1667cb544ba2dfb64ee1
SergioBravo <90683590+SergioBravo@users.noreply.github.com>
2021-10-08 09:45:43 +0200

    Add files via upload

--- DWES-PHP/bingo.php
+++ DWES-PHP/bingo.php
@@ -8,15 +8,39 @@
 
       #CREAMOS LOS CARTONES
       $cartones = crearCartones($bombo);
+      #MOSTRAMOS LOS CARTONES
+      echo "<table cellspacing=10px>";
+        echo "<tr>";
+        foreach ($cartones as $nombreCarton => $carton) {
+          echo "<td>";
+          echo "<table border=1px>";
+            echo "<h2><legend>$nombreCarton</legend></h2>";
+            foreach ($carton as $fila => $columnas) {
+              echo "<tr>";
+              foreach ($columnas as $numero => $valor) {
+                echo "<td>$valor</td>";
+              }
+              echo "</tr>";
+            }
+          echo "</table>";
+          echo "</td>";
+        }
+        echo "</tr>";
+      echo "</table>";
       #CREAMOS EL BOMBO CAMBIANDO DE MANERA ALEATORIA DE NUEVO LOS NÚMEROS DEL MISMO
       shuffle($bombo);
+      #MOSTRAMOS EL BOMBO
+      mostrarBombo($bombo);
       #CREAMOS EL JUEGO COMPARANDO LOS NÚMEROS QUE VAYAN SALIENDO DEL BOMBO CON LOS DEL CARTON
-      $vueltas1 = compararNumero($cartones["carton1"],$bombo);
-      $vueltas2 = compararNumero($cartones["carton2"],$bombo);
-      $vueltas3 = compararNumero($cartones["carton3"],$bombo);
-      $vueltas4 = compararNumero($cartones["carton4"],$bombo);
       #GANADOR
-      //Comparamos todos los valores para saber quien ha dado menos vueltas y por tanto es el ganador
+      $minVueltas = 60;
+      $ganador = "";
+      for ($i=1; $i < 5; $i++) {//Creamos un comparador donde iremos mirando quien ha dado menos vueltas hasta completar el carton y por lo tanto ganar la partida
+        $nombre = "carton".$i;
+        if (compararNumero($cartones[$nombre],$bombo) < $minVueltas) {$minVueltas = compararNumero($cartones[$nombre],$bombo);$ganador = $nombre;}
+      }
+
+      echo "<h2>El ganador del Bingo es <span style=color:red>".$ganador."</span></h2>";
       //Para añadir los espacios en negro tenemos que tener en cuenta si el siguiente al número de cada fila es 2*10 veces mayor que el
       //Seria conveniente hacer una tabla con una fila que tenga a su vez las tablas de los distintos cartones
       #FINAL CODIGO
@@ -48,7 +72,7 @@ function crearCartones($bombo) {//Creamos los cartones puede haber x cartones
   return $cartones;
 }
 
-function compararNumero($array,$bombo){
+function compararNumero($array,$bombo){//Comprobamos cuantas vueltas da un carton hasta completarse
   $con = 0;$vueltas = 0;
   foreach ($bombo as $filas => $numeros) {
     foreach ($array as $fila => $columnas) {
@@ -59,5 +83,19 @@ function compararNumero($array,$bombo){
     if ($con < 15) $vueltas++;
   }
   return $vueltas;
+}
+
+function mostrarBombo($bombo) {
+  $con = 0;//Para controlar las columnas de la tabla bombo
+  echo "<table border=1px>";
+  echo "<h2><legend>Orden aparición bombo</legend></h2>";
+    for ($i=0; $i < count($bombo); $i++) {
+      if ($con == 0) echo "<tr>";
+      echo "<td>".$bombo[$i]."</td>";
+      $con++;
+      if ($con == 5){echo "</tr>";$con = 0;}
+    }
+  echo "</table>";
+
 }
  ?>
