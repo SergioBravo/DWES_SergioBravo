d0e6b270c868460958aa75926c2aa5f7ebd376c0
SergioBravo <90683590+SergioBravo@users.noreply.github.com>
2022-02-17 11:47:43 +0100

    Delete DWES-PHP/p03_SergioBravoCarrillo directory

--- DWES-PHP/p03_SergioBravoCarrillo/epatinMVC/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-  //Llamamos al fichero que nos conecta a la base de datos
-  require_once("db/db.php");
-
-  //Llamamos al controlador del login
-  require_once("controlador/conLogin.php");
-?>
